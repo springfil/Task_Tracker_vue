e418345529a8cc66864ca4abb39c619d8d4ff6b1
springfil <st.kenzhaev@gmail.com>
2023-06-30 02:46:05 +0300

    add components

---
+++ src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+    <header>
+        <h1> {{title}} </h1>
+        <Button text = 'Add Task' color = 'green'/>
+    </header>
+</template>
+
+<script>
+import Button from "./Button";
+
+export default {
+    name: 'Header',
+    props: {
+        title: {
+            type: String,
+            default: 'Aboba'
+        }
+    },
+    components:{
+        Button
+    },
+   
+}
+</script>
+
+// scoped говорит что это предназначено только для этого компонента
+<style scoped>
+    header{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin-bottom: 20px;
+    }
+</style>
